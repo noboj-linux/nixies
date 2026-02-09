012c4b61359fc73daf983450f329795a77416593
noboj-linux <latukhingosha@gmail.com>
2026-02-09 11:59:43 +0300

    Nix flakes & Lockfile

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  
+  outputs = { self, nixpkgs, home-manager, ... }: {
+     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+         system = "x86_64-linux";
+         modules = [
+            ./configuration.nix
+
+            home-manager.nixosModules.home-manager
+            {
+               home-manager.useGlobalPkgs = true;
+               home-manager.useUserPackages = true;
+               home-manager.backupFileExtension = "backup";
+               home-manager.users.nic = { pkgs, ... }: {
+                 home.stateVersion = "25.11";
+                 home.packages = with pkgs; [
+                   tree
+                 ];
+
+               programs.bash.enable = true;
+                 };
+              }
+           ];    
+       }; 
+   };
+}
